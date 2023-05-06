ec7f5f63221d57a125fec3cb19337c9eea6999cd
Erhan <erhantezer68@gmail.com>
2023-05-06 23:29:03 +0300

    updateed app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import { useEffect } from "react";
+import { useState } from "react"
 
 
 function App() {
-  
+  const [loading, setLoading] = useState(false);
+
+  const getFetch = () => {
+    setLoading(true)
+    try {
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+    }
+  };
+
+  useEffect(() => {
+    getFetch()
+  }, []);
+
+
+  if(loading) {
+    console.log("hello")
+  }
 
   return (
     <>
